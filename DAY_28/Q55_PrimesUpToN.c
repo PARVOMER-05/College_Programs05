8b13ae95ef0749eeae4a85d43def00bef77eb6bd
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-06 19:03:56 +0530

    initial commit

---
+++ DAY_28/Q55_PrimesUpToN.c
@@ -0,0 +1,27 @@
+//PROGRAM NO = 55 PROGRAM TO PRINT ALL THE PRIME NUMBERS FROM 1 TO N.
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int num = 2; num <= n; num++) {
+        int isPrime = 1;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            printf("%d ", num);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
