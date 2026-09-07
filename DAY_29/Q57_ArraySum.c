6b1321abbc44bbec3e6b3cf9d3a6c43e0c246241
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-07 19:00:04 +0530

    initial commit

---
+++ DAY_29/Q57_ArraySum.c
@@ -0,0 +1,21 @@
+//PROGRAM NO 57 = FIND THE SUM OF ARRAY ELEMENTS.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
