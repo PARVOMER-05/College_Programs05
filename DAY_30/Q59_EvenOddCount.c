fbe53161e7549b8d38d294ba1ee59c7bdd1cf1e0
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-08 17:42:43 +0530

    initial commit

---
+++ DAY_30/Q59_EvenOddCount.c
@@ -0,0 +1,25 @@
+//PROGRAM NO 59 = COUNT EVEN AND ODD NUMBERS IN AN ARRAY.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int even = 0, odd = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    printf("Even=%d, Odd=%d\n", even, odd);
+
+    return 0;
+}
