b32afcbcdba4d5fa11ffd853cc9552d9d773a49e
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-08 17:46:29 +0530

    initial commit

---
+++ DAY_30/Q60_PosNegZeroCount.c
@@ -0,0 +1,27 @@
+//PROGRAM NO = 60 COUNT POSITIVE , NEGATIVE , AND ZERO ELEMENTS IN AN ARRAY.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int pos = 0, neg = 0, zero = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 0) {
+            pos++;
+        } else if (arr[i] < 0) {
+            neg++;
+        } else {
+            zero++;
+        }
+    }
+
+    printf("Positive=%d, Negative=%d, Zero=%d\n", pos, neg, zero);
+
+    return 0;
+}
