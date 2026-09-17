49162f6d40ff576d334c8559fc7af46d4715c340
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-17 16:01:35 +0530

    commit initial

---
+++ DAY_35/Q70_RotateRight.c
@@ -0,0 +1,29 @@
+//PROGRAM NO = 70 ROTATE AN ARRAY TO THE RIGHT BY K POSITIONS.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int k;
+    scanf("%d", &k);
+
+    k = k % n;
+
+    int temp[n];
+    for (int i = 0; i < n; i++) {
+        temp[(i + k) % n] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", temp[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
