eb8ab0904263e2139b0fe5631d864e7b4c6f2a27
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-17 15:53:43 +0530

    initial commit

---
+++ DAY_34/Q68_DeleteElement.c
@@ -0,0 +1,26 @@
+//PROGRAM NO = 68 DELETE AN ELEMENT FROM AN ARRAY.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int pos;
+    scanf("%d", &pos);
+
+    for (int i = pos; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
