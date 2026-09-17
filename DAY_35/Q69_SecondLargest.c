975730a7ffe75753573d552352a10c81e6e26673
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-17 15:57:56 +0530

    initial commit

---
+++ DAY_35/Q69_SecondLargest.c
@@ -0,0 +1,27 @@
+//PROGRAM NO = 69 FIND THE SECOND LARGEST ELEMENT IN AN ARRAY.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0], secondLargest = -2147483648;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    printf("%d\n", secondLargest);
+
+    return 0;
+}
