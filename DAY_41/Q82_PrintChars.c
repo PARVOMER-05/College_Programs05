f10b39c2238eadfd63a85ca7e10793e4be2abc90
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-19 23:43:25 +0530

    initial commit

---
+++ DAY_41/Q82_PrintChars.c
@@ -0,0 +1,15 @@
+//PROGRAM NO = 82 PRINT EACH CHARACTER OF A STRING ON A NEW LINE.
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+
+    int i = 0;
+    while (str[i] != '\0' && str[i] != '\n') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+
+    return 0;
+}
