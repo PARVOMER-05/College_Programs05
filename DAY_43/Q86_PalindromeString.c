5455c886e992b62ecb90e2e42d7493d108b699bd
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-21 08:41:31 +0530

    initial commit

---
+++ DAY_43/Q86_PalindromeString.c
@@ -0,0 +1,28 @@
+//PROGRAM NO = 43 CHECK IF A STRING IS A PALINDROME.
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+
+    int len = 0;
+    while (str[len] != '\0' && str[len] != '\n') {
+        len++;
+    }
+
+    int isPalindrome = 1;
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome) {
+        printf("Palindrome\n");
+    } else {
+        printf("Not palindrome\n");
+    }
+
+    return 0;
+}
