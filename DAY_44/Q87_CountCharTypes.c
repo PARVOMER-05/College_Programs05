f9eaa5c96c97cecba7a6cb825e265c9be56649b4
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-22 07:01:40 +0530

    initial commit

---
+++ DAY_44/Q87_CountCharTypes.c
@@ -0,0 +1,28 @@
+//PROGRAM NO = 87 COUNT SPACES , DIGITS , AND SPECIAL CHARACTERS IN A STRING.
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+
+    int spaces = 0, digits = 0, special = 0;
+    int i = 0;
+
+    while (str[i] != '\0' && str[i] != '\n') {
+        char ch = str[i];
+
+        if (ch == ' ') {
+            spaces++;
+        } else if (ch >= '0' && ch <= '9') {
+            digits++;
+        } else if (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))) {
+            special++;
+        }
+
+        i++;
+    }
+
+    printf("Spaces=%d, Digits=%d, Special=%d\n", spaces, digits, special);
+
+    return 0;
+}
