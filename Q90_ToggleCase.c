456fc9b5c3c69d321b672001c89d8309c15b9b7a
PARVOMER-05 <Parv.32606@stu.upes.ac.in>
2026-09-23 07:53:46 +0530

    initial commit

---
+++ Q90_ToggleCase.c
@@ -0,0 +1,21 @@
+//PROGRAM NO = 90 TOGGLE CASE OF EACH CHARACTER IN A STRING.
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+
+    int i = 0;
+    while (str[i] != '\0' && str[i] != '\n' && str[i] != '\r') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 'a' + 'A';
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] - 'A' + 'a';
+        }
+        printf("%c", str[i]);
+        i++;
+    }
+    printf("\n");
+
+    return 0;
+}
